80a86ff64f1b415b713a5ef55da80771a2536c75
Ravan Hashimov <ravanhashimov@gmail.com>
2023-06-15 14:03:23 +0300

    Cleanup

--- RankingApp/ClientApp/src/components/Home.js
+++ RankingApp/ClientApp/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,20 +6,64 @@ export class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
+          cupiditate fuga laborum minus natus placeat provident quae quibusdam
+          ratione totam? Ad consectetur ea exercitationem facere magnam quo quos
+          ullam velit veritatis! Aliquam architecto blanditiis consequatur
+          consequuntur dolores expedita fugiat laudantium minima minus non quia
+          quo rerum soluta suscipit, voluptates! Error fugiat quasi sunt
+          voluptatum? Exercitationem fuga hic in quas tempore? Magni, tempora
+          ullam! Eaque eos et harum laboriosam nisi optio, quibusdam soluta
+          unde! Consequuntur, id nam. Earum eum facere ipsa itaque numquam,
+          pariatur perferendis quia repellat sed tenetur. Doloremque illo illum
+          ipsa mollitia numquam quisquam tempora vel! Aperiam aspernatur
+          consequuntur cumque deleniti deserunt distinctio dolor ducimus et
+          excepturi, exercitationem molestiae, non perspiciatis, placeat qui quo
+          quos rerum sapiente sed soluta temporibus tenetur veritatis vitae
+          voluptatem? Dignissimos exercitationem incidunt labore magni?
+          Asperiores dolor facere modi pariatur velit. Assumenda delectus
+          deserunt maxime veniam. Alias deserunt dolor dolores earum labore
+          nihil nobis omnis porro repellendus tempora. Ab alias, culpa debitis
+          eaque eius, eligendi enim expedita in ipsa necessitatibus neque
+          obcaecati pariatur placeat, quasi sit suscipit vel voluptate
+          voluptatem. Ab aliquam animi, consequatur, dolore ea et ex laudantium
+          magni nostrum quas quod sequi, tenetur ut. Accusamus amet autem
+          consequuntur dolore ex ipsam itaque laboriosam maiores, molestiae nisi
+          quisquam ratione. At fugit id illo inventore, magni neque omnis quas
+          repellendus rerum. Assumenda atque consectetur dolor, ducimus eius
+          ipsa natus nemo nisi repellat sequi sit voluptatibus? Ad atque autem
+          corporis cupiditate deleniti doloribus dolorum earum eius enim est
+          excepturi inventore ipsam maiores nihil non, odio odit perferendis
+          perspiciatis placeat praesentium quae quaerat quidem quisquam quos
+          veniam vitae voluptatem! Aperiam aut cum deleniti dicta dolore earum
+          est facilis fuga harum in inventore iste maxime molestiae molestias
+          natus, neque nobis, nulla numquam obcaecati quam quia quidem quod
+          ratione rem saepe sapiente sed similique sint soluta suscipit
+          temporibus tenetur voluptas, voluptatum! Assumenda cumque eligendi
+          facilis fugit iste modi necessitatibus nobis numquam officiis, optio
+          quae quasi ratione rem rerum, sequi sint tempora. Accusamus aliquam
+          architecto aspernatur consequatur cupiditate deleniti distinctio earum
+          eius error esse est eveniet fugit harum id inventore ipsum iure labore
+          libero minima minus modi natus necessitatibus nostrum odio odit optio
+          placeat praesentium quaerat quidem quo repudiandae sapiente, sunt
+          totam ut veniam voluptas voluptate! Ad beatae deserunt ducimus
+          inventore qui rem vel. Alias animi, aperiam architecto atque
+          consequatur, deleniti deserunt dolor dolores earum error fugit iste
+          iure magni modi mollitia nobis obcaecati perferendis quia recusandae
+          rem rerum sint sit soluta ut vitae voluptate voluptatibus voluptatum!
+          Aut, est ex facere libero mollitia obcaecati odio placeat quam, quasi
+          qui unde veniam. Aperiam asperiores error facilis laborum,
+          necessitatibus sed? Ad aperiam asperiores laborum molestias sit
+          soluta. Accusantium aliquid amet, deserunt enim necessitatibus
+          nostrum. Asperiores deserunt, dicta dolore fuga necessitatibus omnis
+          quasi rerum velit. Accusantium commodi debitis, deleniti dolores fuga
+          harum impedit incidunt laboriosam magni nesciunt nostrum, placeat
+          quidem ut! Accusantium architecto facilis ipsa magni provident, sit!
+          Atque deleniti eligendi harum ipsum iure labore laboriosam magni
+          maxime modi nam necessitatibus nemo numquam perspiciatis quasi qui
+          quia quidem, reprehenderit similique sit soluta voluptatem?
+        </span>
       </div>
     );
   }
--- RankingApp/ClientApp/src/components/NavMenu.js
+++ RankingApp/ClientApp/src/components/NavMenu.js
@@ -1,47 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
-export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+const NavMenu = () => {
+  const [collapsed, setCollapsed] = useState(true);
 
-  constructor (props) {
-    super(props);
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
-
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
-
-  render() {
-    return (
+  return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
           <NavbarBrand tag={Link} to="/">RankingApp.API</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+          <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!collapsed} navbar>
             <ul className="navbar-nav flex-grow">
-              <NavItem>
+              <NavItem className=".navLink">
                 <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
+                <NavLink tag={Link} className="text-dark" to="/test">Test</NavLink>
               </NavItem>
             </ul>
           </Collapse>
         </Navbar>
       </header>
-    );
-  }
-}
+  );
+};
+
+export default NavMenu;
